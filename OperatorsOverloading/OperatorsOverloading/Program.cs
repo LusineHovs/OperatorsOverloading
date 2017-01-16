5b4f22d56b6429409864330d750f0f6f6d11daad
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-01-16 16:23:38 +0400

    First Commitment

---
+++ OperatorsOverloading/OperatorsOverloading/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vector1 = new Vector(9);
+            Vector vector2 = new Vector(10);
+            Vector vectorSum = vector1 + vector2;
+            Vector vectorProduct = vector1 * vector2;
+            Console.WriteLine($"The sum of two vectors = {vectorSum}.");
+            Console.WriteLine($"The sum of two vectors = {vectorProduct}.");
+        }
+    }
+}
---
+++ OperatorsOverloading/OperatorsOverloading/Vector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsOverloading
+{
+    class Vector
+    {
+        private double length;
+
+        public Vector()
+        {
+
+        }
+        public Vector(double value)
+        {
+            this.length = value;
+        }
+
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            Vector vector = new Vector();
+            vector.length = a.length + b.length;
+            return vector;
+        }
+
+        public static Vector operator *(Vector c, Vector d)
+        {
+            Vector vector = new Vector();
+            vector.length = c.length * d.length;
+            return vector;
+
+        }
+
+        public override string ToString()
+        {
+            return (String.Format($"{length}"));
+        }
+    }
+}
